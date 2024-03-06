c836a9f2c8830eee6caa6bb6884ad6bf13a20fa9
maski <maskikarsrikar@gmail.com>
2024-03-07 00:39:48 +0530

    added Grab And Rotate Visual cursor, Breathing movement for the shoe

--- src/3dCanvas/Canvas.jsx
+++ src/3dCanvas/Canvas.jsx
@@ -2,7 +2,7 @@ import { Canvas , useThree} from "@react-three/fiber";
 import Model from "./Model";
 import { Environment } from "@react-three/drei";
 import {a,useSpring} from '@react-spring/three'
-import { useMemo } from "react";
+import { useEffect ,useMemo,useState } from "react";
 import * as THREE from 'three'
 import { useDrag } from 'react-use-gesture'
 
@@ -20,22 +20,32 @@ const CanvasModel = () => {
 
 
 function Inspector({ ModelPosition = [0,0,0],responsiveness = 20, children }) {
+    const [hovered, setHovered] = useState(false)
+    const [hold, setHold] = useState(false)
+
+    useEffect(() => {
+      document.body.style.cursor = hold ? 'grabbing' : (hovered && !hold ? 'grab' : 'auto');
+    return () => document.body.cursor = 'auto';
+    }, [hold,hovered]);
+
     const { size } = useThree()
     const euler = useMemo(() => new THREE.Euler(), [])
     const [spring, set] = useSpring(() => ({
       position: ModelPosition,
       rotation: [0, 0, 0],
     }))
-    const bind = useDrag(({ delta: [dx, dy] }) => {
+    const bind = useDrag(({ delta: [dx, dy], dragging }) => {
       euler.y += (dx / size.width) * responsiveness
       euler.x += (dy / size.width) * responsiveness
       euler.x = THREE.MathUtils.clamp(euler.x, -Math.PI / 4, Math.PI / 4)
       set({ rotation: euler.toArray().slice(0, 3) })
+      setHold(dragging)
     })
     return (
-      <a.group {...bind()} {...spring}>
+      <a.group {...bind()} {...spring} onPointerEnter={()=> setHovered(true)} onPointerOut={() => setHovered(false)}>
         {children}
       </a.group>
+
     )
   }
 
--- src/3dCanvas/Model.jsx
+++ src/3dCanvas/Model.jsx
@@ -1,9 +1,17 @@
 import React, { useRef } from 'react'
 import { useGLTF, useTexture } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 
 export default function Model({Position = [0,0,0]}) {
   const groupRef = useRef()
   const { nodes, materials } = useGLTF('/blue_sneaker.glb')
+
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime()
+    groupRef.current.rotation.set(Math.cos(t / 4) / 8, Math.sin(t / 4) / 8, -0.2 - (1 + Math.sin(t / 1.5)) / 20)
+    groupRef.current.position.y = (1+ Math.sin(t/1.5)) /30
+  })
+
   return (
     <group
             ref={groupRef}
